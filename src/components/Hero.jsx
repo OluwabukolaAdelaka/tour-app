01434a2b04aa0eecf80b5eba1e8c7d157e891f13
Grace Adelaka <adelakagrace@gmail.com>
2022-03-30 14:30:27 +0100

    Updated navbar & hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -13,12 +13,16 @@ const Hero = () => {
         <div className='absolute w-full h-full bg-gray-900/30 top-0'></div>
         <div className='absolute w-full h-full flex flex-col top-0 justify-center text-center text-white p-4'>
             <h1>First Class Travel</h1>
-            <h2>Top 1% Locations Worldwide</h2>
+            <h2 className='py-3'>Top 1% Locations Worldwide</h2>
+            <form className='flex justify-between items-center border w-full max-w-[700px] mx-auto p-1 bg-gray-100/90 text-black rounded-md'>
+                <div>
+                    <input className='bg-transparent w-[270px] sm:w-[400px] font-[Poppins] focus:outline-none' type="text" placeholder='Search destinations' />
+                </div>
+                <div>
+                    <button><AiOutlineSearch size={20} className='icon' style={{color:'white'}}/></button>
+                </div>
+            </form>
         </div>
-        <form>
-            <input type="text" />
-            <button><AiOutlineSearch/></button>
-        </form>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -33,14 +33,14 @@ const Navbar = () => {
 
         {/* hamburger */}
         <div onClick={handleNav} className='md:hidden z-10'>
-            {nav ? <AiOutlineClose size={20}/> : <RiMenu4Fill size={20}/>}
+            {nav ? <AiOutlineClose className='text-black' size={20}/> : <RiMenu4Fill size={20}/>}
         </div>
 
       {/* mobile menu */}
       {/* 'absolute left-0 top-0 w-full bg-gray-100/90 py-6 px-4' */}
-      <div className={nav ? 'absolute left-0 top-0 w-full bg-gray-100/90 py-6 px-4' : 
+      <div className={nav ? 'absolute left-0 top-0 w-full bg-gray-100/90 text-black py-6 px-4' : 
       'absolute left-[-100%]'}>
-          <ul >
+          <ul>
               <h1 className='border-b'>BEACHES.</h1>
               <li className='border-b'>Home</li>
               <li className='border-b'>Destinations</li>
